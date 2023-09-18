15e4b17d49b5490bd4312cf8ab8fa24461c0073d
Rouh <roaming.new@gmail.com>
2023-09-18 22:54:17 +0900

    add front

---
+++ rouh-shougi-room-front/src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta name="description" content="" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
